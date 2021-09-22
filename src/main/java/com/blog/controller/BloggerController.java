05e4653018bb1cd05c9c09a4c4864597cbd265ec
jm0820 <18717545422@163.com>
2021-09-22 21:56:54 +0800

    增加博主类

--- src/main/java/com/blog/controller/BloggerController.java
+++ src/main/java/com/blog/controller/BloggerController.java
@@ -1,4 +1,17 @@
 package com.blog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 博主登陆相关
+ */
+@Controller
+@RequestMapping("/blogger")
 public class BloggerController {
+
+    @RequestMapping("/login")
+    public String login(){
+        return null;
+    }
 }
